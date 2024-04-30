1a52f1ef6c7cb51237ff7753a58eb820673236a8
myers25 <144079550+myers25@users.noreply.github.com>
2024-04-29 18:42:44 -0700

    Add files via upload

---
+++ definingFunctions.py
@@ -0,0 +1,8 @@
+def mark():
+    print("Mark is better than you at Python! :)")
+
+name = input("What's your name? ")
+print("Dear " + name, end=", ")
+mark()
+
+input('Press Q to exit')
---
+++ hello.py
@@ -0,0 +1,24 @@
+# Ask user for their name
+name = input("What's your name? ").strip().title()
+
+# List of family members
+family = ['Mark', 'Inga', 'Michael', 'Melody', 'Ralph', 'Letecia']
+
+# Check if the user's name is in the family list
+if name in family:
+    print(f"Hello {name} Myers!")
+else:
+    name2 = input("What is your full name?")
+    print(f"Hello {name2}, you are now a friend of the family!")
+
+question = input("Where are you from? ")
+print("Hello " + name + " from " + question)
+
+question2 = input("How old are you? ")
+print("Hello " + name + " from " + question + " who is " + question2 + " years old.")
+
+question3 = input(" And where do you currently live? ")
+print("Hello " + name + " from " + question + " who is " + question2 + " years old" + " and now lives in " + question3 + ".")
+
+question4 = input("And what about work, what is your job? ")
+print("Hello " + name + " from " + question + " who is " + question2 + " years old" + " and now lives in " + question3 + " and works as a " + question4 + ".")
